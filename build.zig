77f82a1248194e7fb706967343c66021f8522766
Kbz-8 <kbz_8.dev@akel-engine.com>
2025-11-02 20:44:08 +0100

    fixing build

--- build.zig
+++ build.zig
@@ -4,18 +4,14 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    _ = b.addModule("cpuinfo", .{ .root_source_file = b.path("cpuinfo.zig") });
+
     const mod = b.createModule(.{
         .root_source_file = b.path("cpuinfo.zig"),
         .target = target,
         .optimize = optimize,
         .link_libc = (@import("builtin").os.tag == .windows),
     });
-
-    const lib = b.addLibrary(.{
-        .name = "cpuinfo",
-        .root_module = mod,
-    });
-    b.installArtifact(lib);
 
     const test_step = b.addTest(.{ .root_module = mod });
     b.step("test", "Run library tests").dependOn(&b.addRunArtifact(test_step).step);
